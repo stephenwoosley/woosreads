b4eb127d985fe471c23168dd2e0307c52bf871b2
Stephen Woosley <stephenrwoosley@gmail.com>
2017-10-07 13:16:36 -0400

    setting up react & express boilerplate

---
+++ client/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => 
+  <nav className="navbar is-danger" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <a className="navbar-item" href="http://bulma.io">
+        woosreads
+      </a>
+    </div>
+  </nav>;
+
+
+export default Nav;
---
+++ client/src/pages/NoMatch/NoMatch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NoMatch = () => 
+  <div>
+    <h1>NoMatch Page & Component</h1>
+  </div>
+
+
+export default NoMatch;
