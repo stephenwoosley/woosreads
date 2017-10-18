27066b6545fbd301022fdbdeb0c7ba040c9e7042
Stephen Woosley <stephenrwoosley@gmail.com>
2017-10-18 13:08:28 -0400

    edits to the Profile tile

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -2,16 +2,31 @@ import React from "react";
 
 const Profile = () =>
   <article className="tile is-child notification is-primary">
-    <p className="title">
-      <span className="icon is-small is-left">
-        <i className="fa fa-flash"></i>
-      </span>
-      <span className="tile-title">Gabriella Woosley</span>
-    </p>
-    <p className="subtitle">Reading Wizard</p>
-    <figure className="is-image is-128x128">
-      <img className="g-image"src="http://res.cloudinary.com/stephenrwoosley/image/upload/v1507595613/g-tough_ztntg7.png"></img>
-    </figure>
+    <div className="columns">
+      <div className="column is-three-fifths">
+        <figure className="is-image is-128x128">
+          <img className="g-image"src="http://res.cloudinary.com/stephenrwoosley/image/upload/v1507595613/g-tough_ztntg7.png"></img>
+        </figure>
+        <p className="title">Gabriella Woosley</p>
+          {/* <span className="icon is-small is-left">
+            <i className="fa fa-flash"></i>
+          </span> */}
+          {/* <span className="tile-title">Gabriella Woosley</span> */}
+        <p className="subtitle">reading wizard</p>
+      </div>
+      <div className="column">
+        <p className="books-read">
+          <div className="is-size-4 books-read-title">
+            <strong>Books Read:</strong>
+          </div>
+          <div className="books-read-number">
+            <span className="tag is-danger is-large">
+              80
+            </span>
+          </div>
+        </p>
+      </div>
+    </div>
   </article>
 
 
