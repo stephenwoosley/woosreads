5d86e6e48da39c4e1082b0d61ba1055825c98f15
Stephen Woosley <stephenrwoosley@gmail.com>
2017-10-20 14:01:53 -0400

    form working now

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -82,21 +82,38 @@ const Form = props =>
               value={props.rating}
               onChange={props.handleInputChange}
             >
-              <input type="radio" id="star5" name="rating" value="5" /><label className = "full" for="star5" title="Awesome - 5 stars"></label>
-              <input type="radio" id="star4half" name="rating" value="4.5" /><label className="half" for="star4half" title="Pretty good - 4.5 stars"></label>
-              <input type="radio" id="star4" name="rating" value="4" /><label className = "full" for="star4" title="Pretty good - 4 stars"></label>
-              <input type="radio" id="star3half" name="rating" value="3.5" /><label className="half" for="star3half" title="Meh - 3.5 stars"></label>
-              <input type="radio" id="star3" name="rating" value="3" /><label className = "full" for="star3" title="Meh - 3 stars"></label>
-              <input type="radio" id="star2half" name="rating" value="2.5" /><label className="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
-              <input type="radio" id="star2" name="rating" value="2" /><label className = "full" for="star2" title="Kinda bad - 2 stars"></label>
-              <input type="radio" id="star1half" name="rating" value="1.5" /><label className="half" for="star1half" title="Meh - 1.5 stars"></label>
-              <input type="radio" id="star1" name="rating" value="1" /><label className = "full" for="star1" title="Sucks big time - 1 star"></label>
-              <input type="radio" id="starhalf" name="rating" value="0.5" /><label className="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+              <input type="radio" id="star5" name="rating" value="5" /><label className = "full" for="star5" title="Awesome!"></label>
+              <input type="radio" id="star4half" name="rating" value="4.5" /><label className="half" for="star4half" title="Great!"></label>
+              <input type="radio" id="star4" name="rating" value="4" /><label className = "full" for="star4" title="Pretty Good!"></label>
+              <input type="radio" id="star3half" name="rating" value="3.5" /><label className="half" for="star3half" title="It Was Decent."></label>
+              <input type="radio" id="star3" name="rating" value="3" /><label className = "full" for="star3" title="It Was OK."></label>
+              <input type="radio" id="star2half" name="rating" value="2.5" /><label className="half" for="star2half" title="Not Great."></label>
+              <input type="radio" id="star2" name="rating" value="2" /><label className = "full" for="star2" title="Kinda Bad."></label>
+              <input type="radio" id="star1half" name="rating" value="1.5" /><label className="half" for="star1half" title="Not Good."></label>
+              <input type="radio" id="star1" name="rating" value="1" /><label className = "full" for="star1" title="Not Good At All."></label>
+              <input type="radio" id="starhalf" name="rating" value="0.5" /><label className="half" for="starhalf" title="Very Bad!"></label>
             </fieldset>
           </div>
         </div>
       </div>
-      <br></br>
+      <div className= "field is-horizontal">
+      <div className="field-label is-normal">
+          <label className="label has-text-white is-paddingless">Favorite?</label>
+        </div>
+        <div className="field-body">
+          <div className="field">
+            <fieldset
+              className="favorite" 
+              name="favorite"
+              value={props.favorite}
+              onChange={props.handleInputChange}
+              onClick={props.flipFavorite}
+            >
+              <input type="radio" id="bookmark" name="favorite" /><label className = "favorite-bookmark" for="bookmark" title="Bookmark Me!"></label>
+            </fieldset>
+          </div>
+        </div>
+      </div>
       <div className="field is-horizontal">
         <div className="field-label is-normal">
           <label className="label has-text-white is-paddingless">Notes</label>
