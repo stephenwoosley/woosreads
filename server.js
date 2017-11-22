48e862961a865b94dfdad61445e75770d6ec98c2
Stephen Woosley <stephenrwoosley@gmail.com>
2017-11-22 17:34:50 -0500

    Building for production

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  // process.env.MONGODB_URI || 
+  process.env.MONGODB_URI || 
   "mongodb://localhost:27017/woosreads",
   {
     useMongoClient: true
